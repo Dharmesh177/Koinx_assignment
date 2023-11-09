67677d263b4ae582ae656a9d4bf29416d15b7fa6
Dharmesh Vala <dvala453@gmail.com>
2023-11-09 20:14:01 +0530

    task done finally

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Landing_Page from './components/landing-page/Landing_Page';
-import BottomCard from './components/bottom-card/BottomCard';
+import Task from './Task';
 
 function App() {
   return (
     <div className="App">
-    <Header/>
-    <Landing_Page/>
-    <BottomCard/>
-    <Footer/>
+    <Task/>
     </div>
   );
 }
---
+++ src/Task.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Landing_Page from './components/landing-page/Landing_Page';
+import BottomCard from './components/bottom-card/BottomCard';
+
+function Task() {
+  return (
+    <div>
+    <Header/>
+    <Landing_Page/>
+    <BottomCard/>
+    
+    </div>
+  )
+}
+
+export default Task
